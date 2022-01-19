4ce5aea8ae95368b277a0cbacc411392be074fa4
Elias Bruhn <elias@lfpost.dk>
2022-01-19 15:19:32 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <a href="https://bosselijah.github.io">Home</a>
+    <img src={logo} className="app-logo app-logo-1" alt="logo" />
+    <img src={logo} className="app-logo app-logo-2" alt="logo" />
+    <img src={logo} className="app-logo app-logo-3" alt="logo" />
+    <img src={logo} className="app-logo app-logo-4" alt="logo" />
+    <img src={logo} className="app-logo app-logo-5" alt="logo" />
+  </div>
+);
 
 export default App;
